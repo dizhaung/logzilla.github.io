b1e6b722fe395766a9e991794e499082a4992cfb
LogZilla Corporation <logzilla@users.noreply.github.com>
2017-05-21 21:09:48 -0400

    updated for https

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -20,7 +20,7 @@ $(document).ready(function() {
         if(validate()) {
             $.ajax({
                 type: "POST",
-                url: "http://dev.logzilla.net/mailer.php",
+                url: "https://post.logzilla.net/mailer.php",
                 data: form.serialize(),
                 dataType: "json"
             }).done(function(data) {
