3ccc7f5c4a59841c1bda92ad194787e8b207d946
Clayton Dukes <cdukes@logzilla.net>
2017-05-23 14:02:39 -0400

    Removed lightbox, replaced with bs modal Added content to platform.html

--- _site/assets/js/image-fullscreen.js
+++ _site/assets/js/image-fullscreen.js
@@ -1,17 +1,7 @@
-$( document ).ready(function() {
-    $('.img-fullscreen' ).each(function( index ) {
-        src = $(this).attr("src");
-        // console.log( index + ": " + $( this ).text() );
-        // console.log("image source = " + src);
-        if(!$(this).hasClass("img-fluid")){
-            $(this).addClass("img-fluid");
-        }
-        $(this).before('<!-- Start: Image fullscreen -->\n');
-        $(this).wrap('<a href="' + src + '" data-toggle="lightbox"></a>');
-        $(this).append("<!-- End: Image fullscreen -->\n");
-    });
-    $(document).on('click', '[data-toggle="lightbox"]', function(event) {
-            event.preventDefault();
-            $(this).ekkoLightbox({alwaysShowClose: true});
+$(function() {
+    $( ".img-fullscreen" ).wrap( "<a href='#' class='pop'></a>" );
+    $('.pop').on('click', function() {
+        $('.imagepreview').attr('src', $(this).find('img').attr('src'));
+        $('#imagemodal').modal('show');
     });
 });
