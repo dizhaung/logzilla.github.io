07e3906d3c0165b787dc20b5165ac3ac1efc7ab7
Clayton Dukes <cdukes@logzilla.net>
2017-05-24 02:15:20 -0400

    More work on NL

---
+++ _site/assets/js/newsletter.js
@@ -0,0 +1,44 @@
+$(document).ready(function() {
+    console.log("newsletter signup");
+    var form = $('#newsletter');
+    name = "signup-user";
+    email = $('#emailNL');
+    message = "Please add me to your mailing list";
+    company = "NA";
+    phone = "12345";
+    subject = "Newsletter Subscriber";
+    submit = $("#submitNL");
+    submit.on('click', function(e) {
+        e.preventDefault();
+        if(validate()) {
+            $.ajax({
+                type: "POST",
+                url: "https://post.logzilla.net/mailer.php",
+                data: form.serialize(),
+                dataType: "json"
+            }).done(function(data) {
+                if(data.success) {
+                    email.val('');
+                    // subject.val('');
+                    // message.val('');
+                    // company.val('');
+                    // phone.val('');
+                    console.log("newsletter succeeded");
+                } else {
+                    console.log("newsletter failed");
+                }
+            });
+        }
+    });
+
+    function validate() {
+        var valid = true;
+        var regex = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+        if(!regex.test(email.val())) {
+            email.css('border-color', 'red');
+            $("#emailNL").attr("placeholder", "ERROR: Please provide a valid email address").blur();
+            valid = false;
+        }
+        return valid;
+    }
+});
