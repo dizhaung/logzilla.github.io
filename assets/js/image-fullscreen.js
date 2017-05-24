7d4cc145db573b887619596e0992b21fad0e68f8
Clayton Dukes <cdukes@logzilla.net>
2017-05-24 12:14:48 -0400

    Fixed broken links, added zoom cursor to image-fullscreen css

--- assets/js/image-fullscreen.js
+++ assets/js/image-fullscreen.js
@@ -1,5 +1,5 @@
 $(function() {
-    $( ".img-fullscreen" ).wrap( "<a href='#' class='pop'></a>" );
+    $( ".img-fullscreen" ).wrap( "<a href='#' class='pop' style='cursor: -moz-zoom-in; cursor: -webkit-zoom-in; cursor: zoom-in'></a>" );
     $('.pop').on('click', function() {
         $('.imagepreview').attr('src', $(this).find('img').attr('src'));
         $('#imagemodal').modal('show');
