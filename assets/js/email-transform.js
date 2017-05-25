9fb4ceb1f1f10cccba16785afe7b7312b0ec5b69
Clayton Dukes <cdukes@logzilla.net>
2017-05-25 00:41:40 -0400

    Moved most things to use include statements

--- assets/js/email-transform.js
+++ assets/js/email-transform.js
@@ -1,6 +1,8 @@
 $(function() {
-    $('a').each(function() {
+    $('.emailtrans').each(function() {
         var text = $(this).text().replace(/nospam/g, 'logzilla');
+        var href = $(this).attr('href').replace(/nospam/g, 'logzilla');
         $(this).html(text);
+        $(this).attr('href', href);
     });
 });
