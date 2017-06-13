bbf1a894a8af80c9f1cfcbf577a4b5c6cc090bab
Clayton Dukes <cdukes@logzilla.net>
2017-06-12 23:02:37 -0400

    Updated download and contact forms, changed CTA wording

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -1,6 +1,4 @@
 $(document).ready(function() {
-    // Download Form
-    $("#gform").validate();
     $('#gform-installGuide').hide();
     var pathname = window.location.pathname;
     var submitted=false;
@@ -12,14 +10,16 @@ $(document).ready(function() {
         var phone = $('#phone').val();
         var message = $('#message').val();
         $('#gform').toggle( "explode" );
+        if (email !== "") {
         if (pathname == "/contact.html") {
             // contact page
-            $('#contact-us-title').html('<br /><br /><br />Thank you <span class="highlight">' + fname[0] + '!</span><br />!');
+            $('#contact-us-title').html('<br /><br /><br />Thank you <span class="highlight">' + fname[0] + '!</span><br />');
             $('#contact-us-subheading').html('We have received your message and will reach out shortly.');
         } else {
-            $('#gform-results').html('<br /><br /><br />Way to go <span class="highlight">' + fname[0] + '!</span><br />!');
-            $('#gform-results-sub').html('Get ready to be the most well-liked person in your company!');
+            $('#gform-results').html('<br /><br /><br />Way to go <span class="highlight">' + fname[0] + '!</span>');
+            $('#gform-results-sub').html('Please check your email for the download instructions.<br />Get ready to be the most well-liked person in your company!');
         }
-        $('#gform-installGuide').toggle( "puff" );
+        //$('#gform-installGuide').toggle( "puff" );
+     }
     });
 });
